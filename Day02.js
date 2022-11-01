8309a25a818fe55de39e466322e8d2f78ebc1d0f
Abdi-01 <abdialghi@gmail.com>
2022-11-01 20:25:33 +0700

    day-02

--- Day02.js
+++ Day02.js
@@ -28,3 +28,98 @@ console.log("Logic OR :", false || false);
 // NOT
 console.log("Logic NOT :", !true);
 console.log("Logic NOT :", !false);
+
+// TRUTHLY and FALSY
+// TRUTHLY
+console.log(Boolean(2)); // true
+console.log(Boolean("ABC")); // true
+console.log(Boolean(true)); // true
+console.log(Boolean(-2)); // true
+console.log(Boolean(" "));
+
+// FALSY
+console.log(Boolean(false));
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+
+//////////////////////// CONDITIONAL STATEMENT /////////////////////////
+/**
+ * Aturan : 
+ * 1. Membutuhkan nilai boolean
+ * 
+ * Jenis :
+ * 1. IF STATEMENT : untuk 1 kondisi
+ * 2. IF...ELSE STATEMENT : untuk 2 kondisi
+ * 3. IF...ELSE IF.....ELSE : untuk banyak kondisi
+ * 4. SWITCH...CASE STATEMENT: untuk memastikan nilai suatu value
+ */
+let nama = "Leonardo", usia = 15, email = "leo@mail.com";
+
+// 1. IF STATEMENT
+/**
+ * if(condition){
+ *      // task
+ * }
+ */
+if (usia >= 17) {
+    console.log("Usia lebih dari 17thn");
+}
+
+// 2. IF...ELSE STATEMENT
+/**
+ * if(condition){
+ *      // task
+ * }else{
+ *      // task
+ * }
+ */
+
+if (usia >= 17) {
+    console.log(`Verifikasi usia ${nama} berhasil ✅, periksa email ${email}`);
+} else {
+    console.log(`Verifikasi usia ${nama} gagal ❌`);
+}
+
+// 3. IF...ELSE IF.....ELSE STATEMENT
+/**
+ * if(condition){
+ *      // task
+ * }else if(condition2){
+ *      // task
+ * }else{
+ *      // task
+ * }
+ */
+
+let nilai = 80;
+
+if (nilai > 90) {
+    console.log("Grade A", nilai);
+} else if (nilai >= 80 && nilai <= 90) {
+    console.log("Grade B", nilai);
+} else {
+    console.log("Grade C", nilai);
+}
+
+// 4. SWITCH...CASE STATEMENT
+let profesi = "Polisi";
+switch (profesi) {
+    case "Polisi":
+        // task
+        console.log("Saya nilang");
+        break;
+    case "Dokter":
+        console.log("Saya mengobati");
+        break;
+    default:
+        console.log("Tidak tau");
+        break;
+}
+
+// Ternary operator : condition ? nilai : nilai 
+let validasi_email = email.includes('@') ? "Email Benar" : "Email Salah";
+
+
+/////////////////////// LOOP STATEMENT

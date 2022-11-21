2d69421edd97271bab7b05ccadfe646c06552fe0
Abdi-01 <abdialghi@gmail.com>
2022-11-21 21:11:07 +0700

    day12

--- Day12.test.js
+++ Day12.test.js
@@ -1,3 +1,5 @@
+// Install jest : npm install -g jest
+
 // Format penamaan file testing
 // Cara 1 : namaFile.test.js --> paling umum digunakan
 // Cara 2 : namaFile.spec.js
@@ -15,6 +17,19 @@ const { add, multiple } = require('./Day12');
 // .toBe(value) : apakah output yg diberikan sesuai dengan yg diharapkan, utk primitive data type
 // .toEqual(value) : apakah output yg diberikan sesuai dengan yg diharapkan, utk non-primitive data type
 
+// Running testing file from terminal
+// jest --config=config.json file.test
 test('Test add function', () => {
+    // Correct
     expect(add(10, 5)).toBe(15);
 });
+
+test('Test add function Wrong', () => {
+    // wrong
+    expect(add(10, 5)).toBe(6);
+});
+
+test('Test multiple function', () => {
+    expect(multiple(10, 5)).toBe(50);
+});
+
